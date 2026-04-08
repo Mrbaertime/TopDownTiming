82d6ce3fc6915c9cc58ec864f664d5d476375296
Mrbaertime <beartime00@gmail.com>
2026-04-08 15:05:06 +0700

    obj and light

---
+++ Assets/Script/Map/OBJ.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OBJ : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 👉 ถ้าเป็น Enemy → ไม่ให้ชน (ให้ทะลุ)
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Collider2D enemyCol = collision.collider;
+            Collider2D myCol = GetComponent<Collider2D>();
+
+            Physics2D.IgnoreCollision(enemyCol, myCol);
+        }
+    }
+}
